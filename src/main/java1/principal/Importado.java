c38ab625c4eafccb5da95779ff3ffa59124d655d
Alvaro Remache <alvaro1997_14@hotmail.com>
2021-09-24 20:41:03 -0500

    dos

--- src/main/java1/principal/Importado.java
+++ src/main/java1/principal/Importado.java
@@ -1,74 +1,72 @@
 package principal;
 
-public class Nacional {
-
+public class Importado {
 	private String marca;
 	private String modelo;
 	private String anio;
 	private String placa;
 	private int kilometraje;
-	private String codigo;
+	private String cdimporte;
+	private String valor;
 	
-	
-	public Nacional(String m, String mm, String a, String pl, int kl, String cd) {
-		marca = m;
-		modelo = mm;
-		anio = a;
-		placa = pl;
-		kilometraje = kl;
-		codigo = cd;
-
+	public Importado(String m1 , String m2, String a2 , String pl2 , int kl2  , String ci, String vl) {
+		marca =m1;
+		modelo =m2;
+		anio =a2;
+		placa= pl2;
+		kilometraje =kl2;
+		cdimporte=ci;
+		valor=vl;
+		
 	}
-
+	
+	
+	
+	
 	public String getMarca() {
 		return marca;
 	}
-
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-
 	public String getModelo() {
 		return modelo;
 	}
-
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
-
 	public String getAnio() {
 		return anio;
 	}
-
 	public void setAnio(String anio) {
 		this.anio = anio;
 	}
-
 	public String getPlaca() {
 		return placa;
 	}
-
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
-
 	public int getKilometraje() {
 		return kilometraje;
 	}
-
 	public void setKilometraje(int kilometraje) {
 		this.kilometraje = kilometraje;
 	}
-
-	public String getCodigo() {
-		return codigo;
+	public String getCdimporte() {
+		return cdimporte;
 	}
-
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
+	public void setCdimporte(String cdimporte) {
+		this.cdimporte = cdimporte;
 	}
-
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	} 
+	
+	
 	
 
-	
 }
--- src/main/java1/principal/Main.java
+++ src/main/java1/principal/Main.java
@@ -7,13 +7,24 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Scanner sc2 = new Scanner(System.in);
+		Scanner sc3 = new Scanner(System.in);
+		Scanner sc4 = new Scanner(System.in);
+		Scanner sc5 = new Scanner(System.in);
+
 		int opcion;
-		String E1, E2, E3, E4 , E6;
+		String E1, E2, E3, E4;
 		int E5;
+		String E6;
 		String teclado;
+		String teclado2;
 		int pocicionDisponible = 0;
+		int pocicionDisponible2 = 0;
+		String B1, B2, B3, B4, B6, B7;
+		int B5;
+
 		Nacional vector[] = new Nacional[2];
-		
+		Importado vector2[] = new Importado[3];
+
 		do {
 			System.out.println("CONCESIONARIO AMBACAR");
 			System.out.println("Escoja opcion: ");
@@ -25,12 +36,11 @@ public class Main {
 			opcion = sc.nextInt();
 
 			if (opcion == 1) {
-				
+
 				System.out.println(" a)Auto Nacional");
 				System.out.println(" b)Auto Importado ");
 				teclado = sc2.nextLine();
-				
-	
+
 				if (teclado.equals("a")) {
 					System.out.println("ingrese los datos");
 					System.out.println("ingrese Marca");
@@ -44,20 +54,50 @@ public class Main {
 					System.out.println("Kilometraje ");
 					E5 = sc2.nextInt();
 					System.out.println("ingrese codigo nacional ");
-					E6 = sc2.nextLine();
-					
-					vector[pocicionDisponible] = new  Nacional(E1, E2, E3, E4, E5, E6);
+					E6 = sc3.nextLine();
+
+					vector[pocicionDisponible] = new Nacional(E1, E2, E3, E4, E5, E6);
 					pocicionDisponible++;
+				}
+
+				if (teclado.equals("b")) {
+					System.out.println("ingrese los datos");
+					System.out.println("ingrese Marca");
+					B1 = sc4.nextLine();
+					System.out.println("ingrese Modelo");
+					B2 = sc4.nextLine();
+					System.out.println("ingrese Año fabricación");
+					B3 = sc4.nextLine();
+					System.out.println("ingrese Placa");
+					B4 = sc4.nextLine();
+					System.out.println("Kilometraje ");
+					B5 = sc4.nextInt();
+					System.out.println("ingrese codigo importe ");
+					B6 = sc4.nextLine();
+					System.out.println("ingrese Valor de la importación ");
+					B7 = sc4.nextLine();
+
+					vector2[pocicionDisponible2] = new Importado(B1, B2, B3, B4, B5, B6, B7);
+					pocicionDisponible2++;
 
 				}
-				
-				if (teclado.equals("b")) {
-					
+
+			}
+			if (opcion == 2) {
+				System.out.println("Ingrse numero de placa");
+				teclado2 = sc5.nextLine();
+				if (teclado2.equals(vector)) {
+					for (int i = 0; i < vector.length; i++) {
+						if (vector[i] != null) {
+
+							System.out.println(vector[i].getModelo() + vector[i].getMarca() + vector[i].getKilometraje()
+									+ vector[i].getPlaca());
+						}
+					}
+
 				}
-				}
-				
-			
-			
+
+			}
 
 		} while ((opcion == 5));
 
